22686f855803a96d346599080683bc0ff1d47b5b
Abhishek Dwivedi <abhishekd911@gmail.com>
2016-11-17 19:37:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,52 +21,52 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/bg2.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bg2.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/bg3c.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bg3c.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/blog8.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog8.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/cover2.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'cover2.jpg'));
 });
 
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/fb.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'fb.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/gp.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'gp.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/li.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'li.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/item1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'item1.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/item2.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'item2.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/logo3.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo3.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/me.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'me.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/blog7.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog7.jpg'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
