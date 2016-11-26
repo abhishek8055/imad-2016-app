27feba41356fd576e3f0329fea4fa970dc1c68a6
Abhishek Dwivedi <abhishekd911@gmail.com>
2016-11-26 16:41:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -407,6 +407,10 @@ app.get('/Laravel.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Laravel.jpg'));
 });
 
+app.get('/global.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'global.jpg'));
+});
+
 app.get('/sass.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sass.jpg'));
 });
@@ -418,11 +422,6 @@ app.get('/up.jpg', function (req, res) {
 app.get('/organic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'organic.jpg'));
 });
-
-app.get('/global.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'global.jpg'));
-});
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
