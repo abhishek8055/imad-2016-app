7c62e42af4759526f6a0f567ad147458ce7a5486
Abhishek Dwivedi <abhishekd911@gmail.com>
2016-11-26 15:06:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -402,6 +402,11 @@ app.get('/Laravel.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Laravel.jpg'));
 });
 
+app.get('/sass.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sass.jpg'));
+});
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
