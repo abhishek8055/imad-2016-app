b9cefaf0b057b7bf32139247d0a3069eeb731ce8
Abhishek Dwivedi <abhishekd911@gmail.com>
2016-11-26 16:48:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -423,6 +423,10 @@ app.get('/organic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'organic.jpg'));
 });
 
+app.get('/arctic.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'arctic.jpg'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
